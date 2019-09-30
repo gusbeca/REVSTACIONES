99fa6bf5268ab89b7124a8a9ddc275f8a0b7f8be
Gus BeCa <gusbeca@gmail.com>
2019-09-30 15:57:12 -0500

    find conventional relteted station

--- buscarVecinos.py
+++ buscarVecinos.py
@@ -11,6 +11,9 @@ from mysql.connector import errorcode  #Librerias para conexion a BD mysql para 
 import pymysql
 
 
+import prestodb
+import sys
+from pandas import DataFrame
 
 
 def veci():
@@ -42,11 +45,14 @@ def veci():
 
     a=df.shape
     k=0
+
     for i in range(0,a[0]-1):   #bucle para recorre la lista de estaciones
+        if (i * 100 / a[0]) % 1 == 0:
+            print(i * 100 / a[0], '%')
         coords_1 =(df.iloc[i]['LATITUD'],df.iloc[i]['LONGITUD'])
         distancia=9999999
         vecino= -1
-        for j in range(0,a[0]-1):
+        for j in range(0,a[0]-1): #Bule para buscar la automatica vecina
             k=k+1
             if j!= i:
                 coords_2 = (df.iloc[j]['LATITUD'],df.iloc[j]['LONGITUD'])
@@ -58,15 +64,27 @@ def veci():
                         
             else:
                 pass
+#----------------------------------------------------------------------------------------
+        dfCnvc = lista_convencionales()
+        distancia2 = 9999999
+        vecinoCnvc=-1
+        for q in range(0, dfCnvc.shape[0]-1):# Bucle para buscar la convencional vecina
+            coords_2 = (dfCnvc.iloc[q]['lat'], dfCnvc.iloc[q]['long'])
+            d = geopy.distance.geodesic(coords_1, coords_2).km
+            if d < distancia2:
+                distancia2 = d
+                if d < 2:
+                    vecinoCnvc = int(dfCnvc.iloc[q]['stationid'])
+
         df.iloc[i,df.columns.get_loc('VECINO')]=vecino
         df.iloc[i,df.columns.get_loc('D_VECINO')]=distancia
         estacion=df.iloc[i,df.columns.get_loc('CODIGO_CAT')]
-        q=""" UPDATE estaciones
-              SET VECINO = %s, D_VECINO =%s
-              WHERE CODIGO_CAT = %s """ % (vecino,distancia,estacion)#>
+        q=""" UPDATE automatizacion.estaciones
+              SET VECINO = %s, D_VECINO =%s, Convencional_asociada =%s
+              WHERE CODIGO_CAT = %s """ % (vecino,distancia,vecinoCnvc,estacion)
 
         mySQLCursor.execute(q)
-
+#----------------------------------------------------------------------------------------------
     # FUNCION PARA BUSCAR EL VECIMO MAS CERCANO EN LA MISMA CORRIENTE
 
     for i in range(0, a[0]-1):  # bucle para recorre la lista de estaciones
@@ -93,11 +111,33 @@ def veci():
         df.iloc[i, df.columns.get_loc('D_VECINO_CORRIENTE')] = distancia
         estacion = df.iloc[i, df.columns.get_loc('CODIGO_CAT')]
         
-        q = """ UPDATE estaciones
+        q = """ UPDATE automatizacion.estaciones
                   SET VECINO_CORREINTE = %s, D_VECINO_CORRIENTE =%s
                   WHERE CODIGO_CAT = %s """ % (vecino, distancia, estacion)
 
         mySQLCursor.execute(q)
     cnx.commit()
     cnx.close()
+
+
+def lista_convencionales():
+    """Encuentra si la estacion tiene una estacion convencional asociada y la establece en la BD My sql"""
+    #1. Establecer conexion con repo en casandra atravez de prestodb
+    conn = prestodb.dbapi.connect(
+        host='172.16.50.20',
+        port=8080,
+        user='<REDES>',
+        catalog='<cassandra>',
+        # schema='<raw>',
+    )
+    cur = conn.cursor()
+    #2 Traer la list ade las estaciones convencionales con su geolocalizacion
+    cur.execute("select * from cassandra.raw.stations where brand = 'CONVENTIONAL' LIMIT 10000")
+    df = DataFrame(cur.fetchall())
+    df.columns = cur.description
+    df.columns = [col[0] for col in df.columns]
+    return df
+
+
+
 veci()
